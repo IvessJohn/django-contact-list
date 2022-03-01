9882c964f44b058de17da04bda77dd1949558763
IvessJohn <vanek2v@gmail.com>
2022-03-01 08:06:10 +0200

    Defined the scope of the project

--- README.md
+++ README.md
@@ -1,9 +1,20 @@
 # django-contact-list
-### A basic contact list made by [Ivan "John Ivess" Shyika](https://www.youtube.com/c/JohnIvess).
+### A contact list app made by [Ivan "John Ivess" Shyika](https://www.youtube.com/c/JohnIvess).
 
 ## Functionality:
+- Register and log into individual accounts, each with its own list of contacts
 - Add and remove contacts
 - Edit information of already existing contacts
+
+---
+
+## Roadmap:
+- Basic contact addition and removal
+- Existing contact editing
+- Filtering on the home page (in the contact list)
+- Registration and log in
+- Each customer has a unique contact list
+
 
 ![Demo GIF](publicity/demo.gif)
 
--- contactslist/contactsapp/models.py
+++ contactslist/contactsapp/models.py
@@ -3,13 +3,22 @@ from django.db import models
 
 # Create your models here.
 
+
+class Customer(models.Model):
+    """A model associated with the User. Every Customer has its own list of Contacts (one-to-many relationship)"""
+
+    pass
+
+
 class Contact(models.Model):
+    """A model with contact info. Every Contact is associated with only one Customer at a time."""
+
     RELATIONSHIP_CHOICES = [
-        ('FAMILY', 'Family'),
-        ('FRIENDS', 'Friends'),
-        ('RELATIVES', 'Relatives'),
-        ('COWORKERS', 'Coworkers'),
-        ('BUSINESS', 'Business')
+        ("FAMILY", "Family"),
+        ("FRIENDS", "Friends"),
+        ("RELATIVES", "Relatives"),
+        ("COWORKERS", "Coworkers"),
+        ("BUSINESS", "Business"),
     ]
     name: models.CharField = models.CharField(null=False)
     phone: models.IntegerField = models.IntegerField(blank=False, null=True)
