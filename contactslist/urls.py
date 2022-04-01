3d5ff02b187d0d55dc0de5abab5c92aeca567b3f
IvessJohn <vanek2v@gmail.com>
2022-04-01 07:48:20 +0300

    Add/edit deploy-related files

--- contactslist/urls.py
+++ contactslist/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve 
+
+from . import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('contactsapp.urls'))
+    path('', include('contactsapp.urls')),
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATICFILES_DIRS[0]}), 
 ]
